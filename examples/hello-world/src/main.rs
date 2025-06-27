5c2517e01317290535e2a8fb070ef6fb7676d3db
Po Co <polniolino@gmail.com>
2025-06-27 15:15:52 +0200

    Init project

---
+++ examples/hello-world/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let result = material_core::add(1, 2);
+    println!("Hello, world! {}", result);
+}
---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Material Design in Rust";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    crane.url = "github:ipetkov/crane";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      crane,
+    }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs systems (
+          system:
+          f rec {
+            pkgs = nixpkgs.legacyPackages.${system};
+            lib = pkgs.lib;
+            craneLib = crane.mkLib pkgs;
+          }
+        );
+    in
+    {
+      devShells = forAllSystems (
+        { craneLib, ... }:
+        {
+          default = craneLib.devShell { };
+        }
+      );
+    };
+}
